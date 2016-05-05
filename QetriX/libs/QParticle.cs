7689b4be6b509f17bfbb292c22d3d9c6f063ed46
mikiqex <michal.novak@qex.cz>
2016-05-05 13:44:56 +0200

    Create QParticle.cs

---
+++ QetriX/libs/QParticle.cs
@@ -0,0 +1,16 @@
+namespace com.qetrix.libs
+{
+	/* Copyright (c) QetriX.com. Licensed under MIT License, see /LICENSE.txt file.
+	 * 16.05.05 | Particle Class
+	 */
+
+	using System;
+
+
+	public class QParticle
+	{
+		public QParticle()
+		{
+		}
+	}
+}
