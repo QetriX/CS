5fc8185b2a2973171feda2fb259127bb759c79dc
mikiqex <michal.novak@qex.cz>
2016-05-05 13:43:39 +0200

    Create QApp.cs

---
+++ QetriX/libs/QApp.cs
@@ -0,0 +1,14 @@
+namespace com.qetrix.libs
+{
+	/* Copyright (c) QetriX.com. Licensed under MIT License, see /LICENSE.txt file.
+	 * 2016-01-09 | QApp
+	 */
+
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	public class QApp
+	{
+	}
+}
