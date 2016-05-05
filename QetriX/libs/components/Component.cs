f7edcaa3fba184e56290003ce8f4099d706958bd
mikiqex <michal.novak@qex.cz>
2016-05-05 13:46:37 +0200

    Create Component.cs

---
+++ QetriX/libs/components/Component.cs
@@ -0,0 +1,14 @@
+namespace com.qetrix.libs.components
+{
+	/* Copyright (c) QetriX.com. Licensed under MIT License, see /LICENSE.txt file.
+	 * 16.04.11 | QetriX Component class
+	 */
+
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+	class Component
+	{
+	}
+}
